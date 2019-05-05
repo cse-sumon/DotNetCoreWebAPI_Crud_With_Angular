94cd380b28858bcc7bc5bdfadea199c3445090b1
cse-sumon <sumon.cse94@gmail.com>
2019-05-05 15:30:36 +0600

    backend app are here

---
+++ CrudWithApi_Angular (Backend App)/CrudWithApi_Angular/Models/PaymentDetailContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudWithApi_Angular.Models;
+
+namespace CrudWithApi_Angular.Models
+{
+    public class PaymentDetailContext:DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options):base(options)
+        {
+
+        }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+      
+
+    }
+}
